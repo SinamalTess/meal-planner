a2518b8eeed5fe630d52d9a53eebd3291a078b9a
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-09-30 01:49:18 +0200

    feat: add method to fetch product from food database

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <IngredientItem name="Tomato"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import IngredientItem from './components/IngredientItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    IngredientItem
   }
 }
 </script>
---
+++ src/components/IngredientItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IngredientItem',
+  props: {
+    name: String,
+    img: String
+  },
+  methods: {
+    test: () => {
+      const url = 'https://world.openfoodfacts.org/api/v0/product/737628064502.json'
+      fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data)
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+    }
+  },
+  mounted() {
+    this.test()
+  }
+}
+</script>
