76b9a1e4633641e30d3b6e199bcaa16aefce7fc7
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-09-30 14:48:58 +0200

    feat: add Vuestic and create first components

---
+++ src/components/IngredientList.vue
@@ -0,0 +1,14 @@
+<template>
+  <va-list v-for="(ingredient, index) in ingredients" :key="index">
+    <va-list-item>{{ ingredient.name }}</va-list-item>
+  </va-list>
+</template>
+
+<script setup>
+  import { reactive } from 'vue';
+
+  const ingredients = reactive([
+    {name: 'Tomato'},
+    {name: 'Salad'},
+  ])
+</script>
