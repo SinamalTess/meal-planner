b50241e1e3b98ae46854b978381ea7a6bb3d882d
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-09-30 14:53:46 +0200

    feat: add search bar component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import IngredientList from './components/IngredientList.vue'
+import SearchBar from './components/SearchBar.vue'
 </script>
 
 <template>
   <div>
+    <SearchBar />
     <IngredientList />
   </div>
 </template>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <va-input v-model="value" placeholder="Search Ingredient"></va-input>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const value = ref('')
+</script>
