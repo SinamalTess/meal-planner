7e24349d1c837a608e9599814abbf14f0cd97c31
Tess Lucia Sinamal (personal) <tess.sinamal@gmail.com>
2022-09-30 15:14:28 +0200

    feat: add basic fetch method to retrieve ingredients

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,31 @@
 <template>
-  <va-input v-model="value" placeholder="Search Ingredient"></va-input>
+  <va-input v-model="searchInput" placeholder="Search Ingredient"></va-input>
+  <va-button @click="onSearch">Search</va-button>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
-const value = ref('')
+const searchInput = ref('')
+
+const onSearch = () => {
+  const {value} = searchInput
+  const prefix = 'us.'
+  const url = new URL(`https://${prefix}openfoodfacts.org/cgi/search.pl`)
+  const params = {json:true, action:'process',
+    tagtype_0:	'categories',
+    tag_contains_0:	'contains',
+    tag_0:	value}
+  url.search = new URLSearchParams(params).toString();
+
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+}
+
 </script>
